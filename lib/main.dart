6b344a95ec6912432baa416c99083c7253b94f0f
Dell <5095meshwapatel@email.com>
2024-10-18 22:30:48 +0530

    18/10

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:re_exam/view/screen/home_screen.dart';
+
 import 'package:re_exam/view/screen/splash_screen.dart';
 
 import 'firebase_options.dart';
